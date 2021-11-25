8b6e83ffa9810602a460df2201c92dd5ec2264e3
Joana Ferreira <joana_ferreira@JoanaFerreira.local>
2021-11-25 07:11:36 -0100

    added messages to type - quotes from popular movies

--- src/main/java/org/academiadecodigo/speedTyper/Text/Messages.java
+++ src/main/java/org/academiadecodigo/speedTyper/Text/Messages.java
@@ -2,6 +2,17 @@ package org.academiadecodigo.speedTyper.Text;
 
 public class Messages {
 
-    //Public final static Strings
+    public final static String HARRY_POTTER="Happiness can be found even in the darkest of times, if one only remembers to turn on the light. (Albus Dumbledore, Harry Potter)";
+    public final static String LORD_OF_THE_RINGS="All we have to decide is what to do with the time that is given us.” (Gandalf, Lord of The Rings)";
+    public final static String STAR_WARS="May the Force be with you. (Everyone in Star Wars)";
+    public final static String NARNIA="To defeat the darkness out there, you must defeat the darkness inside yourself. (Coriakin, The Chronicles of Narnia)";
+    public final static String AVATAR="All I ever wanted was a single thing worth fighting for. (Jake Sully, Avatar)";
+    public final static String MATRIX="There's a difference between knowing the path and walking the path. (Morpheus, The Matrix)";
+    public final static String HARRY_POTTER_2="It is our choices, Harry, that show what we truly are, far more than our abilities. (Albus Dumbledore, Harry Potter)";
+    public final static String LORD_OF_THE_RINGS_2="Not all those who wander are lost. (Bilbo Baggins, Lord of The Rings)";
+    public final static String STAR_WARS_2="Do or do not. There is no try. (Yoda, Star Wars)";
+    public final static String NARNIA_2="Things never happen the same way twice (Aslan, he Chronicles of Narnia)";
+    public final static String MATRIX_2="I don’t like the idea that I’m not in control of my life. (Neo, The Matrix)";
+    
     //Se quiser mostrar as mensagens randomly, posso fazer enum com option e mensagens (e a seguir faço random com base no array ou hashmap, mas tenho que ir tendo um set de números já selecionados para não repetir a palavra.
 }
