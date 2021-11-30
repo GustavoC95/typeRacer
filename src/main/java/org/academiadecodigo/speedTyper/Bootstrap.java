a007052bff7cbc95e07ddb33e34c8b96a2b7ce3b
Joana Ferreira <joana_ferreira@JoanaFerreira.local>
2021-11-30 11:29:13 -0100

    Bootstrapped the registration view and controller and instruction view and controller

--- src/main/java/org/academiadecodigo/speedTyper/Bootstrap.java
+++ src/main/java/org/academiadecodigo/speedTyper/Bootstrap.java
@@ -4,9 +4,11 @@ import org.academiadecodigo.speedTyper.Controllers.Controller;
 import org.academiadecodigo.speedTyper.Controllers.Impl.InstructionController;
 import org.academiadecodigo.speedTyper.Controllers.Impl.IntroductionController;
 import org.academiadecodigo.speedTyper.Controllers.Impl.PresentationController;
+import org.academiadecodigo.speedTyper.Controllers.Impl.RegisterController;
 import org.academiadecodigo.speedTyper.Views.Impl.InstructionView;
 import org.academiadecodigo.speedTyper.Views.Impl.IntroductionView;
 import org.academiadecodigo.speedTyper.Views.Impl.PresentationView;
+import org.academiadecodigo.speedTyper.Views.Impl.RegisterView;
 import org.academiadecodigo.speedTyper.Views.View;
 
 public class Bootstrap {
@@ -15,18 +17,21 @@ public class Bootstrap {
 
         Controller introductionController =new IntroductionController();
         Controller presentationController=new PresentationController();
-        //
+        Controller registrationController=new RegisterController();
         Controller instructionController=new InstructionController();
 
         View introductionView=new IntroductionView();
         View presentationView=new PresentationView();
-        //
+        View registrationView=new RegisterView();
         View instructionView=new InstructionView();
 
         introductionController.setView(introductionView);
         introductionController.setNextController(presentationController);
         presentationController.setView(presentationView);
-        //
+        presentationController.setNextController(registrationController);
+        registrationController.setView(registrationView);
+        registrationController.setNextController(instructionController);
+        instructionController.setView(instructionView);
         //
 
         return introductionController;
--- src/main/java/org/academiadecodigo/speedTyper/Controllers/Impl/RegisterController.java
+++ src/main/java/org/academiadecodigo/speedTyper/Controllers/Impl/RegisterController.java
@@ -3,4 +3,11 @@ package org.academiadecodigo.speedTyper.Controllers.Impl;
 import org.academiadecodigo.speedTyper.Controllers.AbstractController;
 
 public class RegisterController extends AbstractController {
+
+    public void init() {
+        while(!super.getView().getMoveNext()){
+            super.init();
+        }
+        super.next();
+    }
 }
