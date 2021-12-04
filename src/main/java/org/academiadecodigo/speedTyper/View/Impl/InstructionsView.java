20937a702cccffbf0e24b0a417ba1358838a2671
Joana Ferreira <joana_ferreira@JoanaFerreira.local>
2021-12-04 13:30:50 -0100

    Changed InstructionsView, without SGFX

--- src/main/java/org/academiadecodigo/speedTyper/View/Impl/InstructionsView.java
+++ src/main/java/org/academiadecodigo/speedTyper/View/Impl/InstructionsView.java
@@ -10,26 +10,27 @@ import java.awt.*;
 
 public class InstructionsView extends AbstractView {
 
-    private Text[] instructions;
+    //private Text[] instructions;
 
     public InstructionsView(){
         super();
-        super.setBackground("InstructionsBackground.png");
+        super.setBackground("InstructionsWithTextEmbedded.png");
+        //super.setBackground("InstructionsBackground.png");
 
-        String[] instructionsText=Instructions.INSTRUCTIONS.split("\n");
+       /* String[] instructionsText=Instructions.INSTRUCTIONS.split("\n");
         instructions=new Text[instructionsText.length];
 
         for (int i=0; i< instructions.length; i++){
             instructions[i]=new Text(PADDING+300, 400+(i*50),instructionsText[i]);
-        }
+        }*/
     }
 
-    @Override
+    /*@Override
     public void show(){
         super.show();
         for(Text instruction:instructions){
             instruction.draw();
             instruction.grow(100,15);
-        }
-    }
+
+    } }*/
 }
