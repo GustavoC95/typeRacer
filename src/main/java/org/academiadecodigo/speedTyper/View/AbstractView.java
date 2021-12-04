489c05dce2b546096751dd97f3796410f2c42667
Joana Ferreira <joana_ferreira@JoanaFerreira.local>
2021-12-04 12:05:58 -0100

    Implemented AbstractView

--- src/main/java/org/academiadecodigo/speedTyper/View/AbstractView.java
+++ src/main/java/org/academiadecodigo/speedTyper/View/AbstractView.java
@@ -1,4 +1,38 @@
 package org.academiadecodigo.speedTyper.View;
 
-public class AbstractView {
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public abstract class AbstractView implements View {
+
+    public final static int PADDING=10;
+
+    private Picture background;
+
+    private boolean moveNext;
+
+    public AbstractView(){
+        moveNext=false;
+    }
+
+    public boolean getMoveNext() {
+        return moveNext;
+    }
+
+    public void setMoveNext(boolean choice){
+        moveNext=choice;
+    }
+
+    public Picture getBackground() {
+        return background;
+    }
+
+    public void setBackground(String path){
+        background=new Picture(PADDING, PADDING, path);
+    }
+
+    @Override
+    public void show(){
+        background.draw();
+    }
+    
 }
