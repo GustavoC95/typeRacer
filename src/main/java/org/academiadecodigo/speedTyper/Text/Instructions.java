b0410c276ab94b997599718dc380fc6e57cb9526
Joana Ferreira <joana_ferreira@JoanaFerreira.local>
2021-12-04 16:13:34 -0100

    Changed the text on the view - corrected 'we'll' to 'will'

--- src/main/java/org/academiadecodigo/speedTyper/Text/Instructions.java
+++ src/main/java/org/academiadecodigo/speedTyper/Text/Instructions.java
@@ -4,7 +4,7 @@ public class Instructions {
 
     public final static String INSTRUCTIONS="You'll see some sentences showing up at the screen. You must type them correctly and the fastest you can. \n" +
             "The fastest you type, more points you win! You'll be able to see your position in the game ranking as the game goes on, \n" +
-            "below the sentences. If you fail to type some characters, a warning we'll appear, and you're not going to be bale to move \n" +
+            "below the sentences. If you fail to type some characters, a warning will appear, and you're not going to be bale to move \n" +
             "to the next sentence until you go back and correct your mistake - and time will not stop counting.\n" +
             "May the force be with you!";
 }
